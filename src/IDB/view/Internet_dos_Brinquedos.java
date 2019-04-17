7c57166dbf6af8675cebba5f7371e8ce333c1cfa
danco <danco@dandan>
2019-04-17 01:32:39 -0300

    Classe "Passar Tela", algumas alterações na classe de execução, adicionando telas "Login" e "ConfPartida"

--- src/IDB/view/Internet_dos_Brinquedos.java
+++ src/IDB/view/Internet_dos_Brinquedos.java
@@ -1,37 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package IDB.view;
 
+import IDB.facade.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author danco
- */
 public class Internet_dos_Brinquedos extends Application {
-    
+
+    static FacadeBackEnd facade;
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Inicio.fxml"));
-        
+
         Scene scene = new Scene(root);
-        
+        stage.setTitle("Início");
         stage.setScene(scene);
         stage.show();
+    }
+
+    static public FacadeBackEnd getFacade() {
+        return facade;
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        facade = new FacadeBackEnd();
         launch(args);
     }
-    
 }
